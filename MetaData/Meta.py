6fa7afe92ac0333d7deea4da69669499cb2293fc
Guimbreon <gui.acs03@gmail.com>
2023-03-27 21:16:25 +0100

    Adding the MetaData project

---
+++ MetaData/Meta.py
@@ -0,0 +1,24 @@
+"""
+
+Script to give us the meta-data of a picture.
+
+"""
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+#video-path
+img = input("What file do you want to analyze?\n>>> ").replace("'","")
+image = Image.open(img)
+all_data = image.getexif()
+
+
+# getting all the data from the image
+
+for tag_id in all_data:
+    # translating all the metadata.
+    tag = TAGS.get(tag_id, tag_id)
+    data = all_data.get(tag_id)
+    # decode bytes
+    if isinstance(data, bytes):
+        data = data.decode(encoding = 'unicode_escape')
+    print(f"{tag:25}: {data}")
---
+++ MetaData/README.md
@@ -0,0 +1,3 @@
+use library: Pillow   -> pip3 install Pillow
+
+This program is used to get all the metadata from pictures. Small project but had fun doing it.
